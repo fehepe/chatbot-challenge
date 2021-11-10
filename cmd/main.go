c2472c38f8ed48e34205028b1a7a109c4c4d6cdf
Fernando <fhernandez@truora.com>
2021-11-09 20:30:59 -0500

    Initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"os"
 
-	"github.com/fehepe/chatbot-challenge/pkg/db/mysql"
-	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	app := fiber.New()
-
-	// err := godotenv.Load("../.env")
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-
-	// dbConn := os.Getenv("CONN_DB")
-
-	db, err := mysql.ConnectDB("root:root@tcp(db:3306)/chat_db")
+	err := godotenv.Load("../.env")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println(db)
+	dbConn := os.Getenv("CONN_DB")
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
-
-	app.Listen(":8080")
 }
